05f246145df08df51a171052e92f4fe316f0cdb3
JMustang <eecfredes@gmail.com>
2021-01-23 20:39:25 -0300

    feat: flat style created

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
   titles: {
     marginTop: "30%",
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
 
   tagLineCTA: {
     textDecorationLine: "underline",
+    fontWeight: "bold",
   },
 
   subtitle: {
