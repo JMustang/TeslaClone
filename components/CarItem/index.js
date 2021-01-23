626602b787de8667a6436baa1dc4e01b51509b8a
JMustang <eecfredes@gmail.com>
2021-01-22 21:37:19 -0300

    car item component is done

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -5,32 +5,36 @@ import { View, Text, ImageBackground } from "react-native";
 import StyledButton from "../StyledButton";
 
 const CarItem = (props) => {
+  const { name, tagLine, tagLineCTA, image } = props;
+
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/img/ModelX.jpeg")}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagLine}
+          <Text style={styles.tagLineCTA}>{tagLineCTA}</Text>
+        </Text>
       </View>
 
-      <StyledButton
-        type="primary"
-        content={"Custom Order"}
-        onPress={() => {
-          console.warn("Custon Order was pressed");
-        }}
-      />
-      <StyledButton
-        type="secondary"
-        content={"Existing Inventory"}
-        onPress={() => {
-          console.warn("Existing Inventory was pressed");
-        }}
-      />
+      <View style={styles.buttonsContainer}>
+        <StyledButton
+          type="primary"
+          content={"Custom Order"}
+          onPress={() => {
+            console.warn("Custon Order was pressed");
+          }}
+        />
+        <StyledButton
+          type="secondary"
+          content={"Existing Inventory"}
+          onPress={() => {
+            console.warn("Existing Inventory was pressed");
+          }}
+        />
+      </View>
     </View>
   );
 };
